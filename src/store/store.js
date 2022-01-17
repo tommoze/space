be5c69dfc87136be5b20f08d5e6434d3acbcd753
tommoze <33844718+tommoze@users.noreply.github.com>
2022-01-17 22:46:10 +0200

    [work in progress] combine non toolikited reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import destinations from "./reducers/destinations";
+import { mobileMenu } from "./reducers/mobileMenu";
+import { crewReducer } from "./reducers/crewReducer";
+import { technologyReducer } from "./reducers/technologyReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = configureStore({
   reducer: {
     destinations,
+    mobileMenu,
+    crewReducer,
+    technologyReducer,
   },
 });
